ed176290cc2380181684ddf19bedbe7d0a41ff7f
Raphael Thanner <r.thanner@zeroseven.de>
2021-05-12 21:31:11 +0200

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Countries',
+    'description' => '',
+    'category' => 'plugin',
+    'author' => 'Raphael Thanner',
+    'author_email' => 'r.thanner@zeroseven.de',
+    'author_company' => 'zeroseven design studios GmbH',
+    'state' => 'alpha',
+    'clearCacheOnLoad' => true,
+    'version' => '0.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-10.4.99',
+        ]
+    ]
+];
