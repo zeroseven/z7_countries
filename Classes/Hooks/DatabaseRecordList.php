2102cebc6e513f91286210ce09cac41b500cc048
Raphael Thanner <r.thanner@zeroseven.de>
2021-05-21 20:32:19 +0200

    Register hook

---
+++ Classes/Hooks/DatabaseRecordList.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zeroseven\Countries\Hooks;
+
+use TYPO3\CMS\Recordlist\RecordList\RecordListHookInterface;
+use Zeroseven\Countries\Service\CountryService;
+use Zeroseven\Countries\Service\TCAService;
+
+class DatabaseRecordList implements RecordListHookInterface
+{
+
+    public function makeClip($table, $row, $cells, &$parentObject)
+    {
+        return $cells;
+    }
+
+    public function makeControl($table, $row, $cells, &$parentObject)
+    {
+        return $cells;
+    }
+
+    public function renderListHeader($table, $currentIdList, $headerColumns, &$parentObject)
+    {
+        if (TCAService::getEnableColumn($table)) {
+            foreach (CountryService::getCountries() ?: [] as $country) {
+                $headerColumns['_CONTROL_'] .= $country['title'];
+            }
+        }
+
+        return $headerColumns;
+    }
+
+    public function renderListHeaderActions($table, $currentIdList, $cells, &$parentObject)
+    {
+        return $cells;
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,11 +2,18 @@
 
 defined('TYPO3_MODE') || die('🐰');
 
+// Database manipulation
 $GLOBALS['TYPO3_CONF_VARS']['DB']['additionalQueryRestrictions'][\Zeroseven\Countries\Database\QueryRestriction\CountryQueryRestriction::class] = ['disabled' => false];
+
+// Add some local extension configuration
 $GLOBALS['TYPO3_CONF_VARS']['USER']['z7_countries']['disallowedTables'] = ['backend_layout', 'be_dashboards', 'be_groups', 'be_users', 'fe_groups', 'fe_users', 'index_config', 'sys_category', 'sys_collection', 'sys_file', 'sys_filemounts', 'sys_file_collection', 'sys_file_metadata', 'sys_file_reference', 'sys_file_storage', 'sys_language', 'sys_log', 'sys_news', 'sys_note', 'sys_redirect', 'sys_template', 'tx_z7countries_country'];
 $GLOBALS['TYPO3_CONF_VARS']['USER']['z7_countries']['cache'] = [];
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][TYPO3\CMS\Core\Imaging\IconFactory::class]['overrideIconOverlay'][] = \Zeroseven\Countries\Hooks\IconFactoryHook::class;
 
+// Register hooks
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][TYPO3\CMS\Core\Imaging\IconFactory::class]['overrideIconOverlay'][] = \Zeroseven\Countries\Hooks\IconFactoryHook::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.db_list_extra.inc']['actions'][] = \Zeroseven\Countries\Hooks\DatabaseRecordList::class;
+
+// Register xclass objects
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Site\Entity\Site::class] = [
     'className' => \Zeroseven\Countries\Xclass\Site::class
 ];
