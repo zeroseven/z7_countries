12342e4d502ded83b77f17865153aed071cd89d9
Raphael Thanner <r.thanner@zeroseven.de>
2021-05-27 08:55:15 +0200

    Allow empty country parameter in language service class

--- Classes/Service/LanguageService.php
+++ Classes/Service/LanguageService.php
@@ -10,18 +10,18 @@ use Zeroseven\Countries\Model\Country;
 
 class LanguageService
 {
-    public static function createBase(SiteLanguage $language, Country $country): UriInterface
+    public static function createBase(SiteLanguage $language, Country $country = null): UriInterface
     {
-        if ($isoCode = $country->getIsoCode()) {
+        if ($country && ($isoCode = $country->getIsoCode())) {
             return $language->getBase()->withPath('/' . $language->getTwoLetterIsoCode() . CountryService::DELIMITER . $isoCode . '/');
         }
 
         return $language->getBase();
     }
 
-    public static function createHreflang(SiteLanguage $language, Country $country): string
+    public static function createHreflang(SiteLanguage $language, Country $country = null): string
     {
-        if ($isoCode = $country->getIsoCode()) {
+        if ($country && ($isoCode = $country->getIsoCode())) {
             return $language->getTwoLetterIsoCode() . '-' . strtoupper($isoCode);
         }
 
