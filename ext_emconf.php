a6db55d0fed73858b674f1a6de478e58e364d71b
Raphael Thanner <r.thanner@zeroseven.de>
2021-05-31 08:47:04 +0200

    Add description in composer configuration file and ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Countries',
-    'description' => '',
+    'description' => 'This TYPO3 extension offers the possibility of a flexible country configuration for single tree content.',
     'category' => 'fe',
     'author' => 'Raphael Thanner',
     'author_email' => 'r.thanner@zeroseven.de',
