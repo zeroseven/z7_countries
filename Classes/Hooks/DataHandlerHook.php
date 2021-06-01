7c8231a720a48468de6db58e57c65a770c1b4f94
Raphael Thanner <r.thanner@zeroseven.de>
2021-06-01 17:28:17 +0200

    Refresh pagetree, if the country configurations have changed on table "pages"

---
+++ Classes/Hooks/DataHandlerHook.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zeroseven\Countries\Hooks;
+
+use TYPO3\CMS\Core\DataHandling\DataHandler;
+use Zeroseven\Countries\Service\TCAService;
+
+class DataHandlerHook
+{
+    /**
+     * Refresh pagetree, if the country configurations have changed on table "pages"
+     *
+     * @param bool $status
+     * @param string $table
+     * @param int $id
+     * @param array $fieldArray
+     * @param DataHandler $dataHandler
+     */
+    public function processDatamap_postProcessFieldArray(bool $status, string $table, int $id, array $fieldArray, DataHandler $dataHandler): void
+    {
+        if (
+            $table === 'pages'
+            && ($config = TCAService::getEnableColumn($table))
+            && !empty(array_intersect($config, array_keys($fieldArray)))
+        ) {
+            $dataHandler->pagetreeNeedsRefresh = true;
+        }
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -13,6 +13,7 @@ $GLOBALS['TYPO3_CONF_VARS']['USER']['z7_countries']['cache'] = [];
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Imaging\IconFactory::class]['overrideIconOverlay'][] = \Zeroseven\Countries\Hooks\IconFactoryHook::class;
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Recordlist\RecordList\DatabaseRecordList::class]['modifyQuery'][] = \Zeroseven\Countries\Hooks\DatabaseRecordListHook::class;
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.db_list_extra.inc']['actions'][] = \Zeroseven\Countries\Hooks\DatabaseRecordListHook::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \Zeroseven\Countries\Hooks\DataHandlerHook::class;
 
 // Register xclass objects
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Site\Entity\Site::class] = [
