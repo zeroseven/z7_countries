6dd6f214f98c3386721af5846ebc05ca6b069fc0
Raphael Thanner <r.thanner@zeroseven.de>
2022-03-15 10:55:15 +0100

    Create classes for languages and countries in menu

--- Classes/Utility/MenuUtility.php
+++ Classes/Utility/MenuUtility.php
@@ -14,10 +14,198 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 use Zeroseven\Countries\Database\QueryRestriction\CountryQueryRestriction;
+use Zeroseven\Countries\Exception\CountryException;
 use Zeroseven\Countries\Model\Country;
 use Zeroseven\Countries\Service\CountryService;
 use Zeroseven\Countries\Service\LanguageManipulationService;
 use Zeroseven\Countries\Service\TCAService;
+
+abstract class AbstractItem
+{
+    /** @var string */
+    protected $link;
+
+    /** @var string */
+    protected $hreflang;
+
+    /** @var bool */
+    protected $available;
+
+    /** @var bool */
+    protected $active;
+
+    /** @var bool */
+    protected $current;
+
+    public static function makeInstance(SiteLanguage $language, Country $country = null, string $className = null): self
+    {
+        return GeneralUtility::makeInstance($className ?: self::class)->setHreflang(LanguageManipulationService::getHreflang($language, $country));
+    }
+
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getHreflang(): string
+    {
+        return $this->hreflang;
+    }
+
+    public function setHreflang(string $hreflang): self
+    {
+        $this->hreflang = $hreflang;
+
+        return $this;
+    }
+
+    public function isAvailable(): bool
+    {
+        return $this->available;
+    }
+
+    public function setAvailable(bool $available): self
+    {
+        $this->available = $available;
+
+        return $this;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function isCurrent(): bool
+    {
+        return $this->current;
+    }
+
+    public function setCurrent(bool $current): self
+    {
+        $this->current = $current;
+
+        return $this;
+    }
+}
+
+class LanguageItem extends AbstractItem
+{
+    /** @var SiteLanguage */
+    protected $language;
+
+    /** @var array */
+    protected $countries;
+
+    public static function makeInstance(SiteLanguage $language, Country $country = null, string $className = null): self
+    {
+        return AbstractItem::makeInstance($language, $country, $className ?: self::class)->setLanguage($language);
+    }
+
+    public function getLanguage(): SiteLanguage
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(SiteLanguage $language): self
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    public function getCountries(): array
+    {
+        return $this->countries;
+    }
+
+    public function hasCountry(Country $country): bool
+    {
+        return isset($this->countries[$country->getUid()]);
+    }
+
+    public function hasCountryItem(CountryItem $countryItem): bool
+    {
+        return $this->hasCountry($countryItem->getCountry());
+    }
+
+    public function addCountryItem(CountryItem $countryItem): self
+    {
+        if(!$this->hasCountryItem($countryItem)) {
+            $this->countries[$countryItem->getCountry()->getUid()] = $countryItem;
+        }
+
+        return $this;
+    }
+}
+
+class CountryItem extends AbstractItem
+{
+    /** @var Country */
+    protected $country;
+
+    /** @var array */
+    protected $languages;
+
+    public static function makeInstance(SiteLanguage $language, Country $country = null, string $className = null): self
+    {
+        if ($country === null) {
+            throw new CountryException('Country object is missing', 1647335435);
+        }
+
+        return AbstractItem::makeInstance($language, $country, $className ?: self::class)->setCountry($country);
+    }
+
+    public function getCountry(): Country
+    {
+        return $this->country;
+    }
+
+    public function setCountry(Country $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getLanguages(): array
+    {
+        return $this->languages;
+    }
+
+    public function hasLanguage(SiteLanguage $language): bool
+    {
+        return isset($this->languages[$language->getLanguageId()]);
+    }
+
+    public function hasLanguageItem(LanguageItem $languageItem): bool
+    {
+        return $this->hasLanguage($languageItem->getLanguage());
+    }
+
+    public function addLanguageItem(LanguageItem $languageItem): self
+    {
+        if(!$this->hasLanguageItem($languageItem)) {
+            $this->languages[$languageItem->getLanguage()->getLanguageId()] = $languageItem;
+        }
+
+        return $this;
+    }
+}
 
 class MenuUtility
 {
@@ -126,23 +314,19 @@ class MenuUtility
         return !empty($language) && $language->getLanguageId() === $this->activeLanguageId;
     }
 
-    protected function getCountryMenuItem(Country $country, SiteLanguage $language = null): array
+    protected function getCountryMenuItem(SiteLanguage $language, Country $country): CountryItem
     {
         $link = $this->createLink($language, $country);
         $available = $link && $this->isAvailableCountry($country);
 
-        return [
-            'data' => $country->toArray(),
-            'object' => $country,
-            'link' => $link,
-            'hreflang' => LanguageManipulationService::getHreflang($language, $country),
-            'available' => $available,
-            'active' => $available && $this->isActiveCountry($country),
-            'current' => $available && $this->isActiveCountry($country) && $this->isActiveLanguage($language)
-        ];
+        return CountryItem::makeInstance($language, $country)
+            ->setLink((string)$link)
+            ->setAvailable($available)
+            ->setActive($available && $this->isActiveCountry($country))
+            ->setCurrent($available && $this->isActiveCountry($country) && $this->isActiveLanguage($language));
     }
 
-    protected function getLanguageMenuItem(SiteLanguage $language, Country $country = null, bool $countryAvailable = null): array
+    protected function getLanguageMenuItem(SiteLanguage $language, Country $country = null, bool $countryAvailable = null): LanguageItem
     {
         if ($countryAvailable === null) {
             $countryAvailable = $this->isAvailableCountry($country);
@@ -150,15 +334,11 @@ class MenuUtility
 
         $available = $countryAvailable && $this->isAvailableLanguage($language);
 
-        return [
-            'data' => $language->toArray(),
-            'object' => $language,
-            'link' => $countryAvailable ? $this->createLink($language, $country) : null,
-            'hreflang' => LanguageManipulationService::getHreflang($language, $country),
-            'available' => $available,
-            'active' => $available && $this->isActiveLanguage($language),
-            'current' => $available && $this->isActiveCountry($country) && $this->isActiveLanguage($language)
-        ];
+        return LanguageItem::makeInstance($language, $country)
+            ->setLink((string)($countryAvailable ? $this->createLink($language, $country) : ''))
+            ->setAvailable($available)
+            ->setActive( $available && $this->isActiveLanguage($language))
+            ->setCurrent($available && $this->isActiveCountry($country) && $this->isActiveLanguage($language));
     }
 
     public function setPageId(int $pageId): self
@@ -175,11 +355,11 @@ class MenuUtility
         foreach ($this->site->getLanguages() as $language) {
             foreach (CountryService::getCountriesByLanguageUid($language->getLanguageId()) as $country) {
                 if (!isset($menu[$country->getUid()])) {
-                    $menu[$country->getUid()] = $this->getCountryMenuItem($country, $language);
+                    $menu[$country->getUid()] = $this->getCountryMenuItem($language, $country);
                 }
 
-                if (!isset($menu[$country->getUid()]['languages'][$language->getLanguageId()])) {
-                    $menu[$country->getUid()]['languages'][$language->getLanguageId()] = $this->getLanguageMenuItem($language, $country, $menu[$country->getUid()]['available']);
+                if(!$menu[$country->getUid()]->hasLanguage($language)) {
+                    $menu[$country->getUid()]->addLanguageItem($this->getLanguageMenuItem($language, $country, $menu[$country->getUid()]->isAvailable()));
                 }
             }
         }
@@ -195,7 +375,7 @@ class MenuUtility
             $menu[$language->getLanguageId()] = $this->getLanguageMenuItem($language);
 
             foreach (CountryService::getCountriesByLanguageUid($language->getLanguageId()) as $country) {
-                $menu[$language->getLanguageId()]['countries'][$country->getUid()] = $this->getCountryMenuItem($country, $language);
+                $menu[$language->getLanguageId()]->addCountryItem($this->getCountryMenuItem($language, $country));
             }
         }
 
