34d23b6da08e80dc6a72dad9e47f192062ab85fd
Raphael Thanner <r.thanner@zeroseven.de>
2022-03-28 10:39:11 +0200

    Update syntax in InlineRecordContainerHook

--- Classes/Hooks/InlineRecordContainerHook.php
+++ Classes/Hooks/InlineRecordContainerHook.php
@@ -9,14 +9,15 @@ use Zeroseven\Countries\Service\IconService;
 
 class InlineRecordContainerHook implements InlineElementHookInterface
 {
-    public function renderForeignRecordHeaderControl_preProcess($parentUid, $foreignTable, array $childRecord, array $childConfig, $isVirtual, array &$enabledControls) {
+    public function renderForeignRecordHeaderControl_preProcess($parentUid, $foreignTable, array $childRecord, array $childConfig, $isVirtual, array &$enabledControls)
+    {
 
     }
 
     public function renderForeignRecordHeaderControl_postProcess($parentUid, $foreignTable, array $childRecord, array $childConfig, $isVirtual, array &$controlItems)
     {
         if ($icon = IconService::getRecordFlagIcon($foreignTable, (int)$childRecord['uid'])) {
-            $controlItems['edit'] =  '<span style="margin:5px 10px">' . $icon->render() . '</span>' . $controlItems['edit'];
+            $controlItems['edit'] = '<span style="margin:5px 10px">' . $icon->render() . '</span>' . $controlItems['edit'];
         }
     }
 }
