c7f2eec960f80569fe80b277345274b5ba84236c
Raphael Thanner <r.thanner@zeroseven.de>
2022-04-26 12:25:45 +0200

    Update TCA syntax

--- Configuration/TCA/tx_z7countries_country.php
+++ Configuration/TCA/tx_z7countries_country.php
@@ -33,9 +33,7 @@ return [
             'config' => [
                 'type' => 'check',
                 'items' => [
-                    '1' => [
-                        '0' => 'LLL:EXT:lang/locallang_core.xlf:labels.enabled'
-                    ]
+                    ['LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.enabled', 1]
                 ]
             ]
         ],
