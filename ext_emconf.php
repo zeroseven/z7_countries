ac0a9c8a11ea3ed7dc9210b66a5bad6b5c0f5d05
Raphael Thanner <r.thanner@zeroseven.de>
2024-06-13 21:28:42 +0200

    Make installable in TYPO3 12

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.4.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.99.99'
+            'typo3' => '10.4.0-12.4.99'
         ]
     ]
 ];
