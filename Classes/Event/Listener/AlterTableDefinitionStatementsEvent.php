f21fc1f7f2df6502d29609098b2111cc768e0649
Raphael Thanner <r.thanner@zeroseven.de>
2024-06-24 17:07:47 +0200

    Update alterTableDefinitionStatement event

--- Classes/Event/Listener/AlterTableDefinitionStatementsEvent.php
+++ Classes/Event/Listener/AlterTableDefinitionStatementsEvent.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Zeroseven\Countries\Event\Listener;
 
+use \TYPO3\CMS\Core\Database\Event\AlterTableDefinitionStatementsEvent as Event;
 use Zeroseven\Countries\Service\TCAService;
 
 class AlterTableDefinitionStatementsEvent
 {
-    public function addCountryConfiguration(\TYPO3\CMS\Core\Database\Event\AlterTableDefinitionStatementsEvent $event): void
+
+    public function __invoke(Event $event): void
     {
         $singleSelectTemplate = PHP_EOL . 'CREATE TABLE %s (' . PHP_EOL . '  %s int(1) DEFAULT \'0\' NOT NULL' . PHP_EOL . ');' . PHP_EOL;
         $multiSelectTemplate = PHP_EOL . 'CREATE TABLE %s (' . PHP_EOL . '  %s varchar(255) DEFAULT \'\' NOT NULL' . PHP_EOL . ');' . PHP_EOL;
