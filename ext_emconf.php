35231c7771f4857fb3727798376393bbd388ca14
lwinterhalder <l.winterhalder@outlook.de>
2026-07-15 15:43:14 +0200

    Require TYPO3 13.4 and PHP 8.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'zeroseven design studios GmbH',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '2.4.0',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99'
+            'typo3' => '13.4.0-13.4.99'
         ]
     ]
 ];
